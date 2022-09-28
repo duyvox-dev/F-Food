50adf5dee9ffd656b5688a6ae709227b95da9e53
duyvox-dev <vobaoduy123@gmail.com>
2022-09-28 21:22:32 +0700

    setup page title
    setup page title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { Helmet } from 'react-helmet';
 import { appRoutes } from './routes/appRoutes';
 function App() {
 	return (
 		<div className='App'>
+			<Helmet>
+				<title>F-Food</title>
+			</Helmet>
 			<BrowserRouter>
 				<Routes>
 					{appRoutes.map((route, index) => {
