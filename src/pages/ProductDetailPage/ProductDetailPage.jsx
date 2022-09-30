55fd7f42b40be2ae9be61d5d3fe6702c47ec487c
AnVT <anvt96@gmail.com>
2022-09-30 21:32:16 +0700

    implement UI Product Detail page

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import './ProductDetailPage.scss';
+import banhmi from '../../assets/image/banhmi.jpg';
+
+// or
+import { Grid } from '@mui/material';
 
 export default function ProductDetailPage() {
-	return <div>ProductDetailPage</div>;
+	let vendor = '7Eleven';
+
+	return (
+		<>
+			<div className='container'>
+				<div className='product-image'>
+					<img src={banhmi} alt='' />
+				</div>
+				<div className='product-detail'>
+					<h1 className='product-name'>Bánh Mì</h1>
+					<div className='line'></div>
+					<div className='product-price'>
+						<span className='price'>24.000 đ</span>
+						<span className='price-sale'>28.000 đ</span>
+						<span className='price-tag-sale'>-15%</span>
+					</div>
+				</div>
+
+				<button className='btn-add-product'>Thêm</button>
+
+				<div className='product-vendor'>
+					<p>
+						Đơn hàng đang được xử lý bởi <span className='vendor-name'> {vendor}</span>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
