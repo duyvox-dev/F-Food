e95d1b260e222c38e4121c137d25c86e4f9aafcc
Trang <vokimtrang2712@gmail.com>
2022-10-07 11:07:41 +0700

    update headerpage

--- src/HOC/header/Header.jsx
+++ src/HOC/header/Header.jsx
@@ -1,44 +1,9 @@
-// import React from 'react';
-
-// export default function Header() {
-// 	return <div className='header'>Header</div>;
-// }
-
 import React from 'react';
 import {} from '@mui/material';
 import { SearchRounded } from '@mui/icons-material';
 import './Header.scss';
-// import './Header.css';
 import Logo from '../../img/logo.png';
 import ProfilePic from '../../img/avatar.png';
-
-// function Header() {
-// 	return (
-// 		<header className='container'>
-// 			<div className='logoPage'>
-// 				<img src={Logo} alt='' className='logo' />
-// 				<p className='nameApp'>F-Food</p>
-// 			</div>
-// 			<div className='inputBox'>
-// 				<SearchRounded className='searchIcon' />
-// 				<input type='text' placeholder='Bạn đang thèm ăn gì?' />
-// 			</div>
-// 			<div className='shoppingCart'>
-// 				<ShoppingCartRounded className='cart' />
-// 				<div className='cart_content'>
-// 					<p>2 </p>
-// 				</div>
-// 			</div>
-// 			<div className='profileContainer'>
-// 				<div className='imgBox'>
-// 					<img src={ProfilePic} alt='' className='profilePic' />
-// 				</div>
-// 			</div>
-// 		</header>
-// 	);
-// }
-
-// export default Header;
 
 function Header() {
 	return (
