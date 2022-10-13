8b68f078d37e1241c5620424025b6e0b79c76164
duyvox-dev <vobaoduy123@gmail.com>
2022-10-13 22:06:02 +0700

    setup google login ui and google libs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import App from './App';
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<App />
+		<GoogleOAuthProvider clientId={clientId}>
+			<App />
+		</GoogleOAuthProvider>
 	</Provider>
 );
