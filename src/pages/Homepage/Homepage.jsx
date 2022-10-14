95893518ee1102796ac5e595eedd8821ef5e603b
duyvox-dev <vobaoduy123@gmail.com>
2022-10-14 15:40:32 +0700

    Update Homepage.jsx

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -8,39 +8,12 @@ import TimeOrderBar from './TimeOrderBar';
 import { Link } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { ProductByCategory } from '../../util/data';
+import GoogleLoginBtn from '../../components/GoogleLoginBtn/GoogleLoginBtn';
 export default function HomePage() {
 	return (
 		<>
 			<Container maxWidth='lg'>
-				<div className='home'>
-					<HomeContainer />
-					<TimeOrderBar />
-					<MenuCategory />
-					{/* <div className='listProductByCategory'>
-						<div className='titleListCategory'>
-							<p>Ưu đãi hấp dẫn</p>
-							<p className='underline-content'></p>
-						</div>
-						<ProductCategory />
-						<div className='btViewAllByCategory'>
-							<button type='button' className='styleButton'>
-								Xem thêm
-							</button>
-						</div>
-					</div> */}
-					<div className='listProductByCategory'>
-						<div className='titleListCategory'>
-							<p>Ưu đãi hấp dẫn</p>
-							<p className='underline-content'></p>
-						</div>
-						<ProductList products={ProductByCategory} />
-						<div className='btViewAllByCategory'>
-							<button type='button' className='styleButton'>
-								Xem thêm
-							</button>
-						</div>
-					</div>
-				</div>
+				<GoogleLoginBtn></GoogleLoginBtn>
 			</Container>
 		</>
 	);
