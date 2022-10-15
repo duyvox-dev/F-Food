f4b640c7ff8663f3a6f0be84131b5902ede4bacf
trinhtamm <110019482+trinhtamm@users.noreply.github.com>
2022-10-15 13:29:07 +0700

    Merge branch 'tam-dev' into develop

--- src/components/GoogleLoginBtn/GoogleLoginBtn.jsx
+++ src/components/GoogleLoginBtn/GoogleLoginBtn.jsx
@@ -3,15 +3,32 @@ import GoogleIcon from '../../assets/icons/google-icon.png';
 import { Button, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import httpClient from '../../api/httpClient';
+import apiLinks from '../../util/apiLink';
+import {loginWithGoogle} from'../../redux/authSlice'
+import { useDispatch } from 'react-redux';
 
 const StyledButton = styled(Button)({
 	background: 'white',
 	color: 'black',
 });
+
+
+
+
 export default function GoogleLoginBtn() {
+	const dispatch= useDispatch()
 	const onSuccess = (res) => {
-		console.log(res.credential);
-		// dispatch(loginViaGoogle(res.tokenId));
+		// const loginProp = {
+		// 	idToken:res.credential
+		// }
+		// httpClient.post({
+		// 	url: apiLinks.customer.login,
+		// 	data: loginProp,
+		//   });
+
+
+		  dispatch(loginWithGoogle(res.credential));
 	};
 	const onError = (res) => {
 		console.log(res);
@@ -19,7 +36,7 @@ export default function GoogleLoginBtn() {
 
 	return (
 		<>
-			<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+			<GoogleOAuthProvider clientId={'336558258554-0kocf8i3i9arsv4ik9h0jc2clft4u36s.apps.googleusercontent.com'}>
 				<GoogleLogin onSuccess={onSuccess} onError={onError} locale='VN' cancel_on_tap_outside={false} useOneTap />
 			</GoogleOAuthProvider>
 		</>
