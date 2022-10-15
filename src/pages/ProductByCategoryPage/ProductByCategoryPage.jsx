093abb08a93cfa67a9f799cfec8d86cca87a6242
AnVT <anvt96@gmail.com>
2022-10-15 18:22:44 +0700

    modify code

--- src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
+++ src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
@@ -16,6 +16,11 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+import InputLabel from '@mui/material/InputLabel';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 const options = [
 	'None',
 	'Atria',
@@ -94,6 +99,7 @@ export default function ProductByCategoryPage() {
 	const open = Boolean(anchorEl);
 	const handleClick = (event) => {
 		setAnchorEl(event.currentTarget);
+		console.log('data: ', event);
 	};
 	const handleClose = () => {
 		setAnchorEl(null);
@@ -113,10 +119,10 @@ export default function ProductByCategoryPage() {
 				sx={{
 					padding: '1rem 0',
 				}}>
-				<div className=''>
+				<div>
 					<div className='header-category'>
 						<img src={IconCacLoaiBanh} alt='' className='icon-category' />
-						<div className='title-category'>CÁC LOẠI BÁNH</div>
+						<div className='title-category'>{cake}</div>
 						<div>
 							<IconButton
 								aria-label='more'
@@ -127,54 +133,87 @@ export default function ProductByCategoryPage() {
 								onClick={handleClick}>
 								<MoreVertIcon />
 							</IconButton>
-							<Menu
-								id='long-menu'
-								MenuListProps={{
-									'aria-labelledby': 'long-button',
-								}}
-								anchorEl={anchorEl}
-								open={open}
-								onClose={handleClose}
-								onChange={handleChange}
-								PaperProps={{
-									style: {
-										maxHeight: ITEM_HEIGHT * 4.5,
-										width: '20ch',
-									},
-								}}>
-								{options.map((option) => (
-									<MenuItem key={option} value={option} selected={option === 'Pyxis'} onClick={handleClose}>
-										{option}
-									</MenuItem>
-								))}
-							</Menu>
-						</div>
-						<KeyboardArrowDownIcon />
-					</div>
-					<div className='listProductByCategory'>
-						<div className='content-product'>
-							{/* <Link to={`/category/${n.id}`} className='category'> */}
-							<div className='content-item'>
-								<img src={BanhMiGaXe} alt='' className='image-product' />
-								<div className='info-product'>
-									<div className='name-product'>Combo Bánh Mì Que Gà Xé Phay Phô Mai và 1 Trà Dâu</div>
-									<div className='product-price-discount'>
-										<div className='product-new-price'>{vndCurrencyFormat(42000)}</div>
-										<div className='discount-percent'>-{discountPercent(42000, 49000)}%</div>
-									</div>
-									<div className='product-bottom'>
-										<div className='product-old-price'>{vndCurrencyFormat(49000)}</div>
-									</div>
-								</div>
-							</div>
-							{/* </Link> */}
-							<CheckoutButton size='large' variant='contained'>
-								Thêm
-							</CheckoutButton>
 						</div>
 					</div>
 				</div>
 			</Container>
+			<FormControl sx={{ m: 1, minWidth: 120 }}>
+				<Select value={cake} onChange={handleChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
+					{options.map((option) => (
+						<MenuItem value={option}>{option}</MenuItem>
+					))}
+				</Select>
+				<FormHelperText>Without label</FormHelperText>
+			</FormControl>
 		</>
+		// <>
+		// <Container
+		// 		maxWidth='lg'
+		// 		sx={{
+		// 			padding: '1rem 0',
+		// 		}}>
+		// 		<div className=''>
+		// 			<div className='header-category'>
+		// 				<img src={IconCacLoaiBanh} alt='' className='icon-category' />
+		// 				<div className='title-category'>CÁC LOẠI BÁNH</div>
+		// 				<div>
+		// 					<IconButton
+		// 						aria-label='more'
+		// 						id='long-button'
+		// 						aria-controls={open ? 'long-menu' : undefined}
+		// 						aria-expanded={open ? 'true' : undefined}
+		// 						aria-haspopup='true'
+		// 						onClick={handleClick}>
+		// 						<MoreVertIcon />
+		// 					</IconButton>
+		// 					<Menu
+		// 						id='long-menu'
+		// 						MenuListProps={{
+		// 							'aria-labelledby': 'long-button',
+		// 						}}
+		// 						anchorEl={anchorEl}
+		// 						open={open}
+		// 						onClose={handleClose}
+		// 						onChange={handleChange}
+		// 						PaperProps={{
+		// 							style: {
+		// 								maxHeight: ITEM_HEIGHT * 4.5,
+		// 								width: '20ch',
+		// 							},
+		// 						}}>
+		// 						{options.map((option) => (
+		// 							<MenuItem key={option} value={option} selected={option === 'Pyxis'} onClick={handleClose}>
+		// 								{option}
+		// 							</MenuItem>
+		// 						))}
+		// 					</Menu>
+		// 				</div>
+		// 				<KeyboardArrowDownIcon />
+		// 			</div>
+		// 			<div className='listProductByCategory'>
+		// 				<div className='content-product'>
+		// 					{/* <Link to={`/category/${n.id}`} className='category'> */}
+		// 					<div className='content-item'>
+		// 						<img src={BanhMiGaXe} alt='' className='image-product' />
+		// 						<div className='info-product'>
+		// 							<div className='name-product'>Combo Bánh Mì Que Gà Xé Phay Phô Mai và 1 Trà Dâu</div>
+		// 							<div className='product-price-discount'>
+		// 								<div className='product-new-price'>{vndCurrencyFormat(42000)}</div>
+		// 								<div className='discount-percent'>-{discountPercent(42000, 49000)}%</div>
+		// 							</div>
+		// 							<div className='product-bottom'>
+		// 								<div className='product-old-price'>{vndCurrencyFormat(49000)}</div>
+		// 							</div>
+		// 						</div>
+		// 					</div>
+		// 					{/* </Link> */}
+		// 					<CheckoutButton size='large' variant='contained'>
+		// 						Thêm
+		// 					</CheckoutButton>
+		// 				</div>
+		// 			</div>
+		// 		</div>
+		// 	</Container>
+		// </>
 	);
 }
