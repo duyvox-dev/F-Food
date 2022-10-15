d461a6b1c5580edc7cab6576b1256f92a65ba33c
duyvox-dev <vobaoduy123@gmail.com>
2022-10-15 13:28:05 +0700

    remove google wrapper

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from './App';
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<GoogleOAuthProvider clientId={clientId}>
-			<App />
-		</GoogleOAuthProvider>
+		<App />
 	</Provider>
 );
