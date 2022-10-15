b55946f07616e59bda65be87e20394ea159a679d
duyvox-dev <vobaoduy123@gmail.com>
2022-10-15 13:31:51 +0700

    Revert "Merge branch 'tam-dev' into develop"
    This reverts commit 17481303d52b4d1b20d7d85d8aa1900658c4c87f, reversing
    changes made to d461a6b1c5580edc7cab6576b1256f92a65ba33c.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -8,13 +8,10 @@ import TimeOrderBar from './TimeOrderBar';
 import { Link } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { ProductByCategory } from '../../util/data';
-import GoogleLoginBtn from "../../components/GoogleLoginBtn/GoogleLoginBtn"
 export default function HomePage() {
-	
 	return (
 		<>
 			<Container maxWidth='lg'>
-				<GoogleLoginBtn></GoogleLoginBtn>
 				<div className='home'>
 					<HomeContainer />
 					<TimeOrderBar />
