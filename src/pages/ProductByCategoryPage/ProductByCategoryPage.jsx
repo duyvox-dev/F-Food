61b6b33ce7dfa1aff554c1015b5cbe06b2890b93
AnVT <anvt96@gmail.com>
2022-10-22 17:05:00 +0700

    implement select category load product

--- src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
+++ src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getProductByCategory } from '../../redux/product';
-import { Container, Typography } from '@mui/material';
+import { Container, Menu, MenuItem, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import { vndCurrencyFormat } from '../../util/currency.util';
 import CartBtn from "../../components/CartBtn/CartBtn"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
 
 const CheckoutButton = styled(Button)({
     display: 'block',
@@ -18,16 +20,55 @@ const CheckoutButton = styled(Button)({
     '&:hover': { backgroundColor: 'rgba(243, 101, 34)' },
 });
 
+
+const ITEM_HEIGHT = 48;
+
 function ProductByCategoryPage(props) {
     const { id } = useParams();
 
     const dispatch = useDispatch()
 
     const { products } = useSelector((state) => state.product)
+    const { categoryList } = useSelector((state) => state.category)
+
+    const [currCategory, setCurrCategory] = useState();
+
+    useEffect(() => {
+        console.log("categoryList: ", categoryList)
+    }, [categoryList])
 
     useEffect(() => {
         dispatch(getProductByCategory(id))
+        //setCurrCategory(categoryList.find((item) => item.id === id))
     }, [id])
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const [categoryName, setCategoryName] = useState()
+
+    const categoryObj = categoryList.find((item) => item.categoryName === categoryName)
+    const categoryId = categoryObj?.id
+
+    const handleClickOption = (event) => {
+        const { myValue } = event.currentTarget.dataset;
+        setCategoryName(myValue);
+        setAnchorEl(null);
+    };
+
+    useEffect(() => {
+        dispatch(getProductByCategory(categoryId))
+    }, [categoryId])
+
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
 
     // UI temporary, gonne be replace by new UI
@@ -44,12 +85,47 @@ function ProductByCategoryPage(props) {
                         Tìm kiếm sản phẩm
                     </Typography>
                 </div>
+                <div style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bolder', color: 'rgba(243, 101, 34)' }}>
+                    <span>{categoryName}</span>
+                    <IconButton
+                        aria-label="more"
+                        id="long-button"
+                        aria-controls={open ? 'long-menu' : undefined}
+                        aria-expanded={open ? 'true' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                    >
+                        <MoreVertIcon />
+                    </IconButton>
+                    <Menu
+                        id="long-menu"
+                        MenuListProps={{
+                            'aria-labelledby': 'long-button',
+                        }}
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        PaperProps={{
+                            style: {
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: '20ch',
+                            },
+                        }}
+                    >
+                        {categoryList.map((option) => (
+                            <MenuItem key={option.id} data-my-value={option.categoryName} onClick={handleClickOption}>
+                                {option.categoryName}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                </div>
+
                 <div className='listProductBySearch'>
                     <div className='titleCategoryBySearch'>
                         {/* <p>Đồ uống</p> */}
                     </div>
                     {products.length === 0 ? (<div><h1>CHƯA CÓ DỮ LIỆU</h1></div>) : products.map((product, key) => (
-                        <div className='content-product'>
+                        <div className='content-product' key={key}>
                             <div className='content-item'>
                                 <img src={product.image} alt='' className='image-product' />
                                 <div className='info-product'>
