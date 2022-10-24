63a0dcdcd51debcf0234fc5aeb892c15920197cb
duyvox-dev <vobaoduy123@gmail.com>
2022-10-24 09:46:46 +0700

    'add to cart on search item

--- src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
+++ src/pages/ProductByCategoryPage/ProductByCategoryPage.jsx
@@ -6,74 +6,82 @@ import { Container, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 import { vndCurrencyFormat } from '../../util/currency.util';
-import CartBtn from "../../components/CartBtn/CartBtn"
+import CartBtn from '../../components/CartBtn/CartBtn';
+import { addToCart } from '../../redux/cartSlice';
 
 const CheckoutButton = styled(Button)({
-    display: 'block',
-    color: 'white',
-    backgroundColor: 'rgba(243, 101, 34)',
-    width: '100px',
-    marginTop: '1rem',
-    boxShadow: 'inherit',
-    '&:hover': { backgroundColor: 'rgba(243, 101, 34)' },
+	display: 'block',
+	color: 'white',
+	backgroundColor: 'rgba(243, 101, 34)',
+	width: '100px',
+	marginTop: '1rem',
+	boxShadow: 'inherit',
+	'&:hover': { backgroundColor: 'rgba(243, 101, 34)' },
 });
 
 function ProductByCategoryPage(props) {
-    const { id } = useParams();
+	const { id } = useParams();
 
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    const { products } = useSelector((state) => state.product)
+	const { products } = useSelector((state) => state.product);
 
-    useEffect(() => {
-        dispatch(getProductByCategory(id))
-    }, [id])
+	useEffect(() => {
+		dispatch(getProductByCategory(id));
+	}, [id]);
 
-
-    // UI temporary, gonne be replace by new UI
-    return (
-        <Container
-            maxWidth='lg'
-            sx={{
-                padding: '1rem 0',
-                position: "relative"
-            }}>
-            <div className='searchpage'>
-                <div className='header-search-page'>
-                    <Typography variant='h4' align='center' fontWeight='bold'>
-                        Tìm kiếm sản phẩm
-                    </Typography>
-                </div>
-                <div className='listProductBySearch'>
-                    <div className='titleCategoryBySearch'>
-                        {/* <p>Đồ uống</p> */}
-                    </div>
-                    {products.length === 0 ? (<div><h1>CHƯA CÓ DỮ LIỆU</h1></div>) : products.map((product, key) => (
-                        <div className='content-product'>
-                            <div className='content-item'>
-                                <img src={product.image} alt='' className='image-product' />
-                                <div className='info-product'>
-                                    <div className='name-product'>{product.name}</div>
-                                    <div className='product-price-discount'>
-                                        <div className='product-new-price'>{vndCurrencyFormat(product.price)}</div>
-                                        {/* <div className='discount-percent'>-{discountPercent(43000, 49000)}%</div> */}
-                                    </div>
-                                    {/* <div className='product-bottom'>
+	// UI temporary, gonne be replace by new UI
+	return (
+		<Container
+			maxWidth='lg'
+			sx={{
+				padding: '1rem 0',
+				position: 'relative',
+			}}>
+			<div className='searchpage'>
+				<div className='header-search-page'>
+					<Typography variant='h4' align='center' fontWeight='bold'>
+						Tìm kiếm sản phẩm
+					</Typography>
+				</div>
+				<div className='listProductBySearch'>
+					<div className='titleCategoryBySearch'>{/* <p>Đồ uống</p> */}</div>
+					{products.length === 0 ? (
+						<div>
+							<h1>CHƯA CÓ DỮ LIỆU</h1>
+						</div>
+					) : (
+						products.map((product, key) => (
+							<div className='content-product'>
+								<div className='content-item'>
+									<img src={product.image} alt='' className='image-product' />
+									<div className='info-product'>
+										<div className='name-product'>{product.name}</div>
+										<div className='product-price-discount'>
+											<div className='product-new-price'>{vndCurrencyFormat(product.price)}</div>
+											{/* <div className='discount-percent'>-{discountPercent(43000, 49000)}%</div> */}
+										</div>
+										{/* <div className='product-bottom'>
 											<div className='product-old-price'>{vndCurrencyFormat(49000)}</div>
 										</div> */}
-                                </div>
-                            </div>
-                            <CheckoutButton size='large' variant='contained'>
-                                Thêm
-                            </CheckoutButton>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            <CartBtn></CartBtn>
-        </Container>
-
-    );
+									</div>
+								</div>
+								<CheckoutButton
+									size='large'
+									variant='contained'
+									onClick={() => {
+										dispatch(addToCart(product));
+									}}>
+									Thêm
+								</CheckoutButton>
+							</div>
+						))
+					)}
+				</div>
+			</div>
+			<CartBtn></CartBtn>
+		</Container>
+	);
 }
 
 export default ProductByCategoryPage;
